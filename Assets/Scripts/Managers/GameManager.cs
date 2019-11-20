5619c8300356aa1a94097725147734a554985e7b
Bast F <bast.frederic@gmail.com>
2019-11-20 17:07:09 +0100

    Updated Serialization mode

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using UI;
+using UnityEngine;
+using Utils;
+
+public class GameManager : Singleton<GameManager> {
+
+    [Header("Prefabs")] 
+    public GameObject TankPrefab;
+    
+    [Header("Data")]
+    public List<TankSetting> TankSettings;
+    
+    [Header("References")] 
+    public GameObject WaypointPrefab;
+    public Transform WaypointContent;
+    public Transform BonusContent;
+    public Transform GridStart;
+    public Transform GridEnd;
+    public List<Transform> TankMeleePositions;
+    public List<Transform> TankDuelPositions;
+
+    [Header("External References")] 
+    public TankLifeUI TankLifeUi;
+    
+    [Header("Parameters")] 
+    public int GridXGap;
+    public int GridZGap;
+    public LayerMask CoverLayer;
+
+    public List<GameObject> TankEntities => _tankEntities.Where(go => go != null).ToList();
+    public List<GameObject> WaypointEntities => _waypointEntities.Where(go => go != null).ToList();
+    public List<GameObject> BonusEntities => _bonusEntities.Where(go => go != null).ToList();
+    public List<GameObject> GameObjects => _gameObjects.Where(go => go != null).ToList();
+
+    private List<GameObject> _tankEntities = new List<GameObject>();
+    private List<GameObject> _waypointEntities = new List<GameObject>();
+    private List<GameObject> _bonusEntities = new List<GameObject>();
+    private List<GameObject> _gameObjects = new List<GameObject>();
+
+    private void Start() {
+        _tankEntities = FindObjectsOfType<TankEntity>().Select(entity => entity.gameObject).ToList();
+        _waypointEntities = FindObjectsOfType<WaypointEntity>().Select(entity => entity.gameObject).ToList();
+        _bonusEntities = FindObjectsOfType<BonusEntity>().Select(entity => entity.gameObject).ToList();
+        _gameObjects = FindObjectsOfType<GameObject>().ToList();
+        GenerateWaypointGrid();
+        GenerateTanks();
+    }
+
+    public void GenerateWaypointGrid() {
+        float positionX = GridEnd.position.x - GridStart.position.x;
+        float positionZ = GridEnd.position.z - GridStart.position.z;
+        int xSign = positionX < 0 ? -1 : 1;
+        int ySign = positionZ < 0 ? -1 : 1;
+        for (int i = 0; i < Mathf.Abs(positionX) / GridXGap; i++) {
+            for (int j = 0; j < Mathf.Abs(positionZ) / GridZGap; j++) {
+                Vector3 position = new Vector3(GridStart.position.x + i * GridXGap * xSign, 
+                    0, GridStart.position.z + j * GridZGap * ySign);
+                if (!position.IsPositionOnNavMesh()) continue;
+                GameObject instantiate = Instantiate(WaypointPrefab, position, Quaternion.identity, WaypointContent);
+                instantiate.name = (int) instantiate.transform.position.x + ", " + (int) instantiate.transform.position.z;
+                _waypointEntities.Add(instantiate);
+            }
+        }
+    }
+    
+    public void AddBonus(GameObject bonusGameObject) {
+        _bonusEntities.Add(bonusGameObject);
+    }
+
+    public void GenerateTanks() {
+        if (TankSettings.Count > TankMeleePositions.Count)
+            throw new Exception("Need more positions for tanks");
+        for (int i = 0; i < TankSettings.Count; i++) {
+            Transform tankPosition = TankMeleePositions[i];
+            GameObject instantiate = Instantiate(TankPrefab, tankPosition.position, Quaternion.identity);
+            instantiate.GetComponent<TankEntity>().InitTank(TankSettings[i]);
+            _tankEntities.Add(instantiate);
+        }
+        TankLifeUi.UpdateUI(_tankEntities);
+    }
+    
+}
---
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+
+namespace Managers {
+	public class SceneManager : Singleton<SceneManager> {
+
+		private void Start() {
+			LoadScene(Properties.Scenes.Menu);
+		}
+
+		private Dictionary<string, object> _parameters;
+		
+		public void UnloadScene(string scene) {
+			if (!UnityEngine.SceneManagement.SceneManager.GetSceneByName(scene).isLoaded) return;
+			ConsoleProDebug.LogToFilter("Unloading " + scene + "...", "Management");
+			StartCoroutine(UnloadSceneAsync(scene));
+		}
+		
+		public void LoadScene(string scene) {
+			if (UnityEngine.SceneManagement.SceneManager.GetSceneByName(scene).isLoaded) return;
+			ConsoleProDebug.LogToFilter("Loading " + scene + "...", "Management");
+			StartCoroutine(LoadSceneAsync(scene));
+		}
+
+		public void ReloadScene(string scene) {
+			ConsoleProDebug.LogToFilter("Reloading " + scene + "...", "Management");
+			StartCoroutine(ReloadSceneAsync(scene));
+		}
+
+		public IEnumerator ReloadSceneAsync(string scene) {
+			yield return UnloadSceneAsync(scene);
+			yield return LoadSceneAsync(scene);
+		}
+		
+		private IEnumerator UnloadSceneAsync(string scene) {
+			AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(scene);
+			//Wait until the last operation fully loads to return anything
+			while (!asyncLoad.isDone) {
+				yield return null;
+			}
+			ConsoleProDebug.LogToFilter(scene + " unloaded !", "Management");
+		}
+
+		private IEnumerator LoadSceneAsync(string scene) {
+			AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+			//Wait until the last operation fully loads to return anything
+			while (!asyncLoad.isDone) {
+				yield return null;
+			}
+			ConsoleProDebug.LogToFilter(scene + " loaded !", "Management");
+		}
+		
+		public object GetParam(string paramKey) {
+			if (_parameters == null) return null;
+			return _parameters.ContainsKey(paramKey) ? _parameters[paramKey] : null;
+		}
+ 
+		public void SetParam(string paramKey, object paramValue) {
+			if (_parameters == null)
+				_parameters = new Dictionary<string, object>();
+			RemoveParam(paramKey);
+			_parameters.Add(paramKey, paramValue);
+		}
+
+		public void RemoveParam(string paramKey) {
+			if (_parameters.ContainsKey(paramKey))
+				_parameters.Remove(paramKey);
+		}
+
+	}
+}
